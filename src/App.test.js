44008ae180366a18badbda2f12efddf28e4ef2ce
Bartosz Sławianowski <eplightning@outlook.com>
2018-02-13 19:55:29 +0100

    Add Provider and Router to test so it can pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createReduxStore } from './utils/create-store';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux';
 
 it('renders without crashing', () => {
+  const history = createHistory();
+  const store = createReduxStore(history);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(<Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>, div);
 });
